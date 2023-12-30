c5b18e5c6cb9003032e4f126c436e1ba44c8611c
Nurlan <nurlanabijbillaev@gmail.com>
2023-12-30 08:13:10 +0600

    test2

---
+++ src/Car.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Car {
+    String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
